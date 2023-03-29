8c5f592f61a5267ca7f032a5e0cefe6885611897
roshik14 <fraust1337@gmail.com>
2023-03-29 12:52:03 +0700

    feat: add day00 solution

---
+++ day00/d00/d00_ex00/Program.cs
@@ -0,0 +1,36 @@
+﻿if (args.Length != 3)
+{
+    Console.WriteLine("Something went wrong. Check your input and retry");
+    return;
+}
+
+var isFirstOk = double.TryParse(args[0], out var sum);
+var isSecondOk = double.TryParse(args[1], out var rate);
+var isThirdOk = int.TryParse(args[2], out var term);
+
+if (!isFirstOk || !isSecondOk|| !isThirdOk || sum <= 0 || rate <= 0 || term <= 0)
+{
+    Console.WriteLine("Something went wrong. Check your input and retry");
+    return;
+}
+
+var i = rate / 12 / 100;
+var payment = sum * i * Math.Pow((1 + i), term) / (Math.Pow(1 + i, term) - 1);
+var dt = DateTime.Now.AddMonths(1);
+var date = new DateTime(dt.Year, dt.Month, 1);
+var remainingDebt = sum;
+
+for (var num = 1; num <= term; num++)
+{
+    var interest = (remainingDebt * rate * (date - date.AddMonths(-1)).TotalDays) / (100 * (DateTime.IsLeapYear(date.Year) ? 365 : 366));
+
+    var principalDebt = payment - interest;
+    remainingDebt -= principalDebt;
+    if (num == term && remainingDebt > 0)
+    {
+        payment += remainingDebt;
+        remainingDebt = 0;
+    }
+    Console.WriteLine($"{num}\t{date:MM/dd/yyyy}\t{payment:N2}\t{principalDebt:N2}\t{interest:N2}\t{remainingDebt:N2}");
+    date = date.AddMonths(1);
+}
---
+++ day00/d00/d00_ex01/Program.cs
@@ -0,0 +1,68 @@
+﻿var names = File.ReadAllLines("us_names.txt");
+
+Console.WriteLine("Enter name:");
+var input = Console.ReadLine()?.Trim();
+if (string.IsNullOrEmpty(input))
+{
+    Console.WriteLine("Something went wrong. Check your input and retry.");
+    return;
+}
+
+var isFound = false;
+
+foreach (var name in names)
+{
+    var distance = GetLevensteinDistance(input, name);
+    if (distance < 2)
+    {
+        if (name == input)
+        {
+            Console.WriteLine($"Hello, {{{name}}}!");
+            isFound = true;
+            break;
+        }
+        Console.WriteLine($"Did you mean \"{{{name}}}\"? Y/N");
+        var answer = Console.ReadLine()?.ToLower();
+        if (string.IsNullOrEmpty(answer))
+        {
+            Console.WriteLine("Something went wrong. Check your input and retry.");
+            return;
+        }
+        if (answer == "y")
+        {
+            Console.WriteLine($"Hello, {{{name}}}!");
+            isFound = true;
+            break;
+        }
+    }
+}
+
+if (!isFound)
+{
+    Console.WriteLine("Your name was not found.");
+}
+
+int GetLevensteinDistance(string first, string second)
+{
+    var opt = new int[first.Length + 1, second.Length + 1];
+    for (var i = 0; i < first.Length; i++)
+    {
+        opt[i, 0] = i;
+    }
+    for (var i = 0; i < second.Length; i++)
+    {
+        opt[0, i] = i;
+    }
+    for (var i = 1; i <= first.Length; i++)
+    {
+        for (var j = 1; j <= second.Length; j++)
+        {
+            opt[i, j] = first[i - 1] == second[j - 1]
+                ? opt[i - 1, j - 1]
+                : Min(0 + opt[i - 1, j], 1 + opt[i - 1, j - 1], 1 + opt[i, j - 1]);
+        }
+    }
+    return opt[first.Length, second.Length];
+}
+
+int Min(params int[] values) => values.Min();
